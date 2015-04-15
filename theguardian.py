8a8288a32bf5c4102de7fddc03e9e77f1b3fdcec
Roy <gregorian.zhang@gmail.com>
2015-04-15 14:42:32 +0800

    add the guardian.py

---
+++ theguardian.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+
+import requests
+from bs4 import BeautifulSoup
+
+urlli = []
+urllib = []
+
+def getnewurl(url):
+    main = requests.get(url)
+    soup = BeautifulSoup(main.text)
+    main1 = soup.find_all('a', class_ = "global-navigation__action")
+    for x in main1:
+        urlli.append(x["href"])
+#        print x["href"]
+    getnewsurl(urlli)
+#    print urlli
+
+def getnewsurl(urllist):
+#    print urllist
+    for a in urllist:
+        print a
+        main = requests.get(a)
+        soup = BeautifulSoup(main.text)
+        main1 = soup.find_all('a', class_ ="u-faux-block-link__overlay")
+        for x in main1:
+            urllib.append(x["href"])
+
+        getnews(urllib,"digital humanity")
+
+    
+
+def getnews(urllist,keyword):
+    for a in urllist:
+        main = requests.get(a)
+        soup = BeautifulSoup(main.text)
+        title = soup.find("h1", class_ ="content__headline").text
+        try:
+            subheading = soup.find("div", class_ ="content__standfirst").p.text
+        except AttributeError:
+            subheading = ""
+        try:
+            maintext = soup.find("div", class_ ="content__article-body").text
+        except AttributeError:
+            maintext = ""
+
+
+    if maintext.find(keyword) != -1:
+        print  maintext
+    else:
+        pass
+
+if __name__ == '__main__':
+    url="http://www.theguardian.com/uk"
+#    getnewurl(url)
+    getnewurl(url)
